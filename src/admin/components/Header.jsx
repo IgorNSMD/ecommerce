dcfe73b0d0639ed896712728b5baa98dd59e0b4b
IgorNSMD <isanmartin131210@hotmail.cl>
2024-03-27 15:14:40 -0300

    Cambio Logo ISM

--- src/admin/components/Header.jsx
+++ src/admin/components/Header.jsx
@@ -13,7 +13,8 @@ export default function Header({ cart, removeFromCart, increaseQty, decreaseQty,
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
                         <a href="index.html">
-                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+                            {/* <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" /> */}
+                            <img className="img-fluid" src="/img/LogoISM.png" alt="imagen logo" /> 
                         </a>
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
